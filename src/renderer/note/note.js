f02eac20e7173cb7178b707c945861fbf41f9516
GabeG0408 <112592199+GabeG0408@users.noreply.github.com>
2025-05-08 07:18:16 -0500

    Update note.js

--- src/renderer/note/note.js
+++ src/renderer/note/note.js
@@ -104,10 +104,9 @@ document.getElementById('hub-button').addEventListener('click', () => {
 document.getElementById('delete-button').addEventListener('click', () => {
     const confirmation = confirm('Are you sure you want to delete this note?');
     if (confirmation) {
-        // Send 'delete-note' message with noteId to the main process
+        // Send the 'delete-note' message with the noteId to the main process
         ipcRenderer.send('delete-note', noteId);
-        // Close the note window after deletion
-        window.close();
+
     }
 });
 
